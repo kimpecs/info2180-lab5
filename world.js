8264784fc05fdd70d50169069fbfc95db8f08e0c
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-11-19 13:20:06 -0500

    added js and updated it

---
+++ world.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const lookupButton = document.getElementById('lookup');
+    const countryInput = document.getElementById('country');
+    const resultDiv = document.getElementById('result');
+
+    lookupButton.addEventListener('click', function () {
+        const country = countryInput.value.trim();
+
+        if (country !== '') {
+            const url = `world.php?country=${encodeURIComponent(country)}`;
+
+            
+            fetch(url)
+                .then(response => response.text())
+                .then(data => {
+                    resultDiv.innerHTML = data;
+                })
+                .catch(error => {
+                    console.error('There was an error fetching the data:', error);
+                    resultDiv.innerHTML = 'An error occurred. Please try again.';
+                });
+        } else {
+            resultDiv.innerHTML = 'Please enter a country name.';
+        }
+    });
+});
